49e61a2f0691739e35be9198fcddcbd8ad9a9bbb
Travis Racisz <travisracisz@Traviss-MacBook-Pro.local>
2022-03-29 16:35:47 +0900

    added more database config vars

--- utils/sequelize.js
+++ utils/sequelize.js
@@ -17,8 +17,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     port: 5432,
     ssl:true, 
     dialectOptions: { 
-        ssl: true, 
-        rejectUnauthorized: false
+        ssl: { 
+            require: true, 
+            rejectUnauthorized: false
+        }, 
     }
   })
 // const sequelize = require("../index")
