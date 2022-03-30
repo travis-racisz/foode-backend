388aa6a2cc78d1bc48bdc5253d2127a26bbb6792
Travis Racisz <travisracisz@Traviss-MacBook-Pro.local>
2022-03-30 09:40:18 +0900

    testing apollo context with hour variable

--- server.js
+++ server.js
@@ -540,7 +540,8 @@ async function startApolloServer(typeDefs, resolvers){
         
         context: ({req}) => { 
             const token = req.headers.authorization
-            const hour = new Date().getHours()
+            // const hour = new Date().getHours()
+            const hour = 10
             return {req: req.body, token: token, hour}
         }
         // plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
