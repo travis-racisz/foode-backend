7f34abb8267a36d0ee9cf13a73013183179498b8
travis-racisz <63677846+travis-racisz@users.noreply.github.com>
2023-03-31 03:01:55 +0900

    removed comments

--- resolvers/mutations/createPaymentIntent.js
+++ resolvers/mutations/createPaymentIntent.js
@@ -1,35 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPESECRET)
 const db = require("../../utils/firebaseConfig")
 const createPaymentIntent = async (_, args, context) => {
-    console.log(args, 'args')
-
-    // calculate the amount on the backend, args needs to accept an array of objects then loop through the objects to get the amount
-    // was already done on the front end shouldnt be too hard 
-
-    // function getOptionsTotal(cart){ 
-    //     const cartOptions = []
-    //     if( cart.length > 0 ) { 
-    //         cart.forEach(item => { 
-    //             if(item.options){ 
-    //                 item.options.forEach((item) => { 
-    //                     cartOptions.push(item.value)
-    //                 })
-    //             }
-    //         })
-    //     }
-    //     const optionsTotal = cartOptions.reduce((prev, acc) => prev + acc)
-    //     return optionsTotal
-    // }
-
-    // function getCartItemsTotal(cart){ 
-    //     const cartTotal = cart.reduce((prev, acc) => { 
-    //         prev + acc.price
-    //     })
-    //     return cartTotal
-    // }
-    
-    // console.log(getCartItemsTotal(args.cart), "cart total")
-    // console.log(getOptionsTotal(args.cart), "options total")
 
     const driversFee = args.total * .2
     const platformFee = (args.total + driversFee) * .07
